81101797fe94954b03d663ed9cfcf6243a60ce6d
Andre Vannucci <adelannucci@gmail.com>
2017-06-26 10:04:19 -0300

    init commit

---
+++ models/usage.js
@@ -0,0 +1,34 @@
+var mongoose = require('mongoose');
+
+var UsageSchema = new mongoose.Schema({
+  time:  {
+    type: Date,
+    required: true
+  },
+  lat: {
+    type: Number,
+    required: true
+  },
+  lng: {
+    type: Number,
+    required: true
+  },
+  so:   {
+    type: String,
+    required: true
+  },
+  version:   {
+    type: String,
+    required: true
+  },
+  macaddress:   {
+      type: String,
+      required: true
+  }, 
+  connections:   {
+    type: Number,
+    required: true
+  },
+});
+
+module.exports = mongoose.model('Usage', UsageSchema);
---
+++ routes/usage.js
@@ -0,0 +1,64 @@
+var express = require('express');
+var router = express.Router();
+var mongoose = require('mongoose');
+
+router.get('/', (req, res, next) => {
+  mongoose.model('Usage').find().then((info) => {
+    res.render('index',{
+      data: info
+    });
+  },next);
+});
+
+router.get('/all', (req, res, next) => {
+  mongoose.model('Usage').find().then((info) => {
+    res.json({
+      data: info
+    });
+  },next);
+});
+
+router.get('/add',(req,res,next)=>{
+  res.render('add');
+
+});
+
+router.post('/add',(req,res,next)=>{
+  var Usage = mongoose.model('Usage');
+  var m = new Usage(req.body);
+
+  m.save().then((result) => {
+    res.json(result);
+  },next);
+
+});
+
+router.post('/',(req,res,next)=>{
+  var Usage = mongoose.model('Usage');
+  var m = new Usage(req.body);
+  console.log(m);
+
+  m.save().then((result) => {
+    res.redirect('/');
+  },next);
+
+});
+
+router.get('/edit/:id',(req,res,next)=>{
+  const {id} = req.params;
+
+  mongoose.model('Usage').findOne({_id: id}).then((info) => {
+    res.render('edit.njk', {data: info});
+  },next);
+});
+
+router.delete('/delete/:id', (req,res,next)=>{
+  const {id} = req.params;
+
+  mongoose.model('Usage').remove({_id: id}).then((info) => {
+    res.redirect('/')
+  },next);
+
+});
+
+module.exports = router;
